760c7cf1384769b1fad1f14859e317ea31bf50ca
grithope <zhunti99999@gmail.com>
2017-04-25 18:49:13 +0800

    seed 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new
+u = User.first
 u.email = 'admin@test.com'
 u.password = '123456'
 u.password_confirmation = '123456'
